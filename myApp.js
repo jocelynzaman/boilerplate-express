b0db4ad1940ca6414059f1f25f5d51039a14e10a
Jocelyn Zaman <jzaman@asu.edu>
2024-09-09 16:48:14 -0700

    use environment variable in response

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello World");
 
@@ -11,7 +12,12 @@ app.get("/", function(req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
+    var response = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase")
+    {
+        response = response.toUpperCase();
+    }
+    res.json({"message": response});
 });
 
 
