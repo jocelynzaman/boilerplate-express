f707ecfa499309c6ca2ff766130d644dff803f70
Jocelyn Zaman <jzaman@asu.edu>
2024-09-09 18:07:41 -0700

    get query parameter input from client
    query string is delimited by ?
    couple consist of field=value
    each couple is separated by &

--- myApp.js
+++ myApp.js
@@ -1,41 +1,46 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+let bodyParser = require('body-parser');
 
 console.log("Hello World");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log(req.method + ' ' + req.path + ' - ' + req.ip);
     next();
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     //res.send("Hello Express");
     res.sendFile(__dirname + "/views/index.html"); //__dirname is module scoped variable; set to file's absolute directory path
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res) {
+app.get("/json", function (req, res) {
     var response = "Hello json";
-    if (process.env.MESSAGE_STYLE === "uppercase")
-    {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
         response = response.toUpperCase();
     }
-    res.json({"message": response});
+    res.json({ "message": response });
 });
 
-app.get('/now', function(req, res, next) {
+app.get('/now', function (req, res, next) {
     req.time = new Date().toString();
     next();
-}, function(req, res) {
-    res.json({"time": req.time});
+}, function (req, res) {
+    res.json({ "time": req.time });
 });
 
-app.get('/:word/echo', function(req, res) {
-    res.json({"echo": req.params.word});
+app.get('/:word/echo', function (req, res) {
+    res.json({ "echo": req.params.word });
 });
 
+app.route('/name')
+    .get(function (req, res) {
+        let userName = req.query.first + ' ' + req.query.last;
+        res.json({ "name": userName });
+    });
 
 
 
@@ -60,7 +65,4 @@ app.get('/:word/echo', function(req, res) {
 
 
 
-
-
-
- module.exports = app;
+module.exports = app;
